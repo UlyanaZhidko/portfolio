55988605d9adf0a5ad1c2528b939682145ca0362
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-08-20 13:38:34 +0300

    20.08

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 new WOW().init();
+
+
+
+/* слайдер */
+
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active',
+        clickable: true
+    },
+});
