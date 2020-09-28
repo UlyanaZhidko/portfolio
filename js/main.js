48df09d74c2fc31a5a9fbc831380ed054dd738b3
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2020-09-28 21:53:29 +0300

    28.09

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ new WOW().init();
 
 
 /* слайдер */
-
 var mySwiper = new Swiper('.swiper-container', {
     pagination: {
         el: '.project-pagination',
@@ -50,3 +49,15 @@ addAd.addEventListener('click', () => {
 modalAdd.addEventListener('click', closeModal);
 
 
+
+/*стрелка вверх*/
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        if ($('#upbutton').is(':hidden')) {
+            $('#upbutton').css({ opacity: 1 }).fadeIn('slow');
+        }
+    } else { $('#upbutton').stop(true, false).fadeOut('fast'); }
+});
+$('#upbutton').click(function () {
+    $('html, body').stop().animate({ scrollTop: 0 }, 300);
+});
