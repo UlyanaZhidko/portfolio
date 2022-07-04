e932bd353ece8e6a4a67578d2cd1fa7d28132837
UlyanaZhidko <Zhidko.Ulyana@gmail.com>
2022-07-04 13:01:34 +0300

    del: swiper(css/js) from projects

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
 'use strict';
 
-
 new WOW().init();
-
-/* слайдер */
-var mySwiper = new Swiper('.swiper-container', {
-    pagination: {
-        el: '.project-pagination',
-        bulletClass: 'project-bullet',
-        bulletActiveClass: 'project-bullet-active',
-        clickable: true
-    },
-});
 
 
 /* Политика конфидициальности */
